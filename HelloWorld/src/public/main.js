7e528cc24b3e73b77c2be0bb97ce295a1907e84c
LunafreyjaS <lschafer21@mail.wou.edu>
2024-04-04 16:21:32 -0700

    Fixed Hello World 3D Graphics (Assignment 1)

--- HelloWorld/src/public/main.js
+++ HelloWorld/src/public/main.js
@@ -1,52 +1,38 @@
 //Example Code from Class Repository 
 //https://github.com/morses/CS407-S24-ClassRepo/blob/main/HelloThreeJS/main.js
 
-import * as THREE from './node_modules/three/build/three.module.js';
-  
-  // Get a reference to the container element that will hold our scene
-  const container = document.querySelector('#scene-container');
-  
-  // create a Scene
-  const scene = new Scene();
-  
-  // Set the background color
-  scene.background = new Color('skyblue');
-  
-  // Create a camera
-  const fov = 35; // AKA Field of View
-  const aspect = container.clientWidth / container.clientHeight;
-  const near = 0.1; // the near clipping plane
-  const far = 100; // the far clipping plane
-  
-  const camera = new PerspectiveCamera(fov, aspect, near, far);
-  
-  // every object is initially created at ( 0, 0, 0 )
-  // move the camera back so we can view the scene
-  camera.position.set(0, 0, 10);
-  
-  // create a geometry
-  const geometry = new BoxGeometry(2, 2, 2);
-  
-  // create a default (white) Basic material
-  const material = new MeshBasicMaterial();
-  
-  // create a Mesh containing the geometry and material
-  const cube = new Mesh(geometry, material);
-  
-  // add the mesh to the scene
-  scene.add(cube);
-  
-  // create the renderer
-  const renderer = new WebGLRenderer();
-  
-  // next, set the renderer to the same size as our container element
-  renderer.setSize(container.clientWidth, container.clientHeight);
-  
-  // finally, set the pixel ratio so that our scene will look good on HiDPI displays
-  renderer.setPixelRatio(window.devicePixelRatio);
-  
-  // add the automatically created <canvas> element to the page
-  container.append(renderer.domElement);
-  
-  // render, or 'create a still image', of the scene
-  renderer.render(scene, camera);
+import * as THREE from '../../node_modules/three/build/three.module.js';     // if this gives you an error it's because you forgot to install three.js :-)
+
+const scene = new THREE.Scene();
+
+scene.background = new THREE.Color('lightblue');
+
+
+
+const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+
+material.wireframe = true;
+
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 3;
+camera.position.y = -.5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+}
+
+animate();
